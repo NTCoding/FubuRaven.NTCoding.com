3bf9f7f40980329296f1f0b498d56062c6e6432a
NTCoding <Nick@NTCoding.co.uk>
2011-09-11 12:11:44 +0100

    Applied action-locating / url-naming conventions for fubu

---
+++ src/app/Web/Configuration/AppStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Routing;
+using FubuMVC.Core;
+using StructureMap;
+using Web.Configuration;
+using BootstrappingExtensions = FubuMVC.StructureMap.BootstrappingExtensions;
+
+// You can remove the reference to WebActivator by calling the Start() method from your Global.asax Application_Start
+[assembly: WebActivator.PreApplicationStartMethod(typeof(AppStart), "Start", callAfterGlobalAppStart: true)]
+
+namespace Web.Configuration
+{
+    public static class AppStart
+    {
+        public static void Start()
+        {
+            // FubuApplication "guides" the bootstrapping of the FubuMVC
+            // application
+            BootstrappingExtensions.StructureMap(FubuApplication.For<NTCodingFubuRegistry>(), new Container())
+                .Bootstrap(RouteTable.Routes);
+        }
+    }
+}
---
+++ src/app/Web/Configuration/NTCodingFubuRegistry.cs
@@ -0,0 +1,32 @@
+﻿using FubuMVC.Core;
+
+namespace Web.Configuration
+{
+    public class NTCodingFubuRegistry : FubuRegistry
+    {
+        public NTCodingFubuRegistry()
+        {
+            // This line turns on the basic diagnostics and request tracing
+            IncludeDiagnostics(true);
+
+            // Policies
+            Routes
+				.IgnoreNamespaceText("EndPoints")
+                .IgnoreMethodSuffix("Html")
+				.IgnoreClassSuffix("EndPoint")
+				.IgnoreMethodsNamed("get")
+				.IgnoreMethodsNamed("post")
+				.ConstrainToHttpMethod(x => x.Method.Name.Equals("Get"), "Get")
+				.ConstrainToHttpMethod(x => x.Method.Name.Equals("Post"), "Post")
+                .RootAtAssemblyNamespace();
+
+        	Actions
+        		.IncludeTypes(t => t.Namespace.Contains("Web.EndPoints") && t.Name.EndsWith("EndPoint"));
+
+
+            // Match views to action methods by matching
+            // on model type, view name, and namespace
+            Views.TryToAttachWithDefaultConventions();
+        }
+    }
+}

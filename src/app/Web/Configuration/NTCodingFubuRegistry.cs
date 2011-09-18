f55b57d3e88046336c101cf50241bf3cc4d1898c
NTCoding <Nick@NTCoding.co.uk>
2011-09-18 15:08:08 +0100

    html convention for dictionaries

--- src/app/Web/Configuration/NTCodingFubuRegistry.cs
+++ src/app/Web/Configuration/NTCodingFubuRegistry.cs
@@ -1,4 +1,6 @@
-﻿using FubuMVC.Core;
+﻿using System;
+using System.Collections.Generic;
+using FubuMVC.Core;
 using FubuMVC.Core.UI.Configuration;
 using FubuMVC.Spark;
 using FubuMVC.Validation;
@@ -49,9 +51,45 @@ namespace Web.Configuration
 						);
 
         	HtmlConvention(x =>
-        	               x.Labels
-        	               	.If(e => e.Accessor.Name.EndsWith("_BigText"))
-        	               	.BuildBy(er => new HtmlTag("label").Text(er.Accessor.Name.Replace("_BigText", "")))
+        					   x.Labels
+        	               		.If(e => e.Accessor.Name.EndsWith("_BigText"))
+        	               		.BuildBy(er => new HtmlTag("label").Text(er.Accessor.Name.Replace("_BigText", "")))
+						);
+
+			HtmlConvention(x =>
+							x.Editors
+							  .If(e => e.Accessor.PropertyType.IsEnum)
+							  .BuildBy(er =>
+						           		{
+						           			var tag = new HtmlTag("select");
+						           			var enumValues = Enum.GetValues(er.Accessor.PropertyType);
+						           			foreach (var enumValue in enumValues)
+						           			{
+						           				tag.Children.Add(new HtmlTag("option").Text(enumValue.ToString()));
+						           			}
+
+						           			return tag;
+						           		})
+						);
+
+			HtmlConvention(x => 
+							x.Editors
+								.If(e => e.Accessor.PropertyType.IsAssignableFrom(typeof(IDictionary<String,String>)))
+								.BuildBy(er =>
+								         	{
+								         		var dictionary = er.Value<IDictionary<String, String>>();
+								         		string name = er.Accessor.PropertyNames[0].Substring(0, er.Accessor.PropertyNames[0].Length - 1);
+								         		var tag = new HtmlTag("select").Attr("name", name);
+								         		foreach (var item in dictionary)
+								         		{
+								         			tag.Children.Add(new HtmlTag("option")
+														.Text(item.Value)
+														.Attr("value", item.Key)
+														);
+								         		}
+
+								         		return tag;
+								         	})
 						);
 
 

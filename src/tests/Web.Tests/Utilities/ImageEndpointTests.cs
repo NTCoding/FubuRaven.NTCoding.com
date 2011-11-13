c4f20e7d0b167a59426a3a89361f1f971bddfae7
NTCoding <Nick@NTCoding.co.uk>
2011-11-13 22:02:57 +0000

    refactor last test

--- src/tests/Web.Tests/Utilities/ImageEndpointTests.cs
+++ src/tests/Web.Tests/Utilities/ImageEndpointTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Model;
 using NUnit.Framework;
 using Web.Endpoints;
 
@@ -13,16 +14,22 @@ namespace Web.Tests.Utilities
 		[Test]
 		public void Get_GivenIdForBook_ShouldReturnBooksImage()
 		{
-			var imageData = new byte[] {1, 2, 3, 5, 6, 8, 9};
-			var book = BookTestingHelper.GetBook(imageData: imageData);
-			Session.Store(book);
-			Session.SaveChanges();
-
 			var endpoint = new ImageEndpoint(Session);
+			
+			var book = GetBookWithImageFromSession();
 
 			var image = endpoint.Get(new ImageLinkModel {Id = book.Id});
 
 			Assert.AreEqual(book.Image, image.Data);
+		}
+
+		private Book GetBookWithImageFromSession()
+		{
+			var imageData = new byte[] {1, 2, 3, 5, 6, 8, 9};
+			var book = BookTestingHelper.GetBook(imageData: imageData);
+			Session.Store(book);
+			Session.SaveChanges();
+			return book;
 		}
 
 		// TODO - verify content type is always a png

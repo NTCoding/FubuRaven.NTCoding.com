3965c8daa1d91723c81043b6bed949e0893ec5c9
NTCoding <Nick@NTCoding.co.uk>
2011-11-19 23:01:02 +0000

    takes a link model test added for update book endpoint

--- src/tests/Web.Tests/Books/UpdateEndpointTests.cs
+++ src/tests/Web.Tests/Books/UpdateEndpointTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace Web.Tests.Books
@@ -5,16 +6,25 @@ namespace Web.Tests.Books
 	[TestFixture]
 	public class UpdateEndpointTests
 	{
+		private UpdateEndpoint endpoint;
 		// GET 
 			// should take a link model with the books id
 			
 			// should return a book view model with book's details
 
-		[Test]
+		[SetUp]
 		public void CanCreate()
 		{
-			new UpdateEndpoint();
+			endpoint = new UpdateEndpoint();
 		}
+
+		[Test]
+		public void Get_ShouldTakeALinkModel_WithBooksId()
+		{
+			endpoint.Get(new UpdateBookLinkModel {Id = "123"});
+		}
+
+		// TODO - should blow up if ID Null
 
 		// POST
 			// TODO - validation / failure scenario
@@ -24,7 +34,15 @@ namespace Web.Tests.Books
 			// Should redirect to the view book page
 	}
 
+	public class UpdateBookLinkModel
+	{
+		public String Id { get; set; }
+	}
+
 	public class UpdateEndpoint
 	{
+		public void Get(UpdateBookLinkModel updateBookLinkModel)
+		{
+		}
 	}
 }

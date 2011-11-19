21188bd4729df9adc4c6868ec872fabcbf68fd23
NTCoding <Nick@NTCoding.co.uk>
2011-11-19 22:40:30 +0000

    added link to site management on home page, created new link model to do so

--- src/app/Web/Endpoints/SiteManagement/IndexEndpoint.cs
+++ src/app/Web/Endpoints/SiteManagement/IndexEndpoint.cs
@@ -2,10 +2,14 @@ namespace Web.Endpoints.SiteManagement
 {
 	public class IndexEndpoint
 	{
-		public SiteManagementIndexViewModel Get()
+		public SiteManagementIndexViewModel Get(SiteManagementLinkModel model)
 		{
 			return new SiteManagementIndexViewModel();
 		}
+	}
+
+	public class SiteManagementLinkModel
+	{
 	}
 
 	public class SiteManagementIndexViewModel

56bcd9ee07d8cea207d93724626e0e79e9e7081d
NTCoding <Nick@NTCoding.co.uk>
2011-11-19 23:21:45 +0000

    added failing test for update book endpoint - need to add inner test for model

--- src/tests/Web.Tests/Books/UpdateEndpointTests.cs
+++ src/tests/Web.Tests/Books/UpdateEndpointTests.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Model;
 using NUnit.Framework;
+using Raven.Client;
+using Web.Tests.Utilities;
 
 namespace Web.Tests.Books
 {
 	[TestFixture]
-	public class UpdateEndpointTests
+	public class UpdateEndpointTests : RavenTestsBase
 	{
 		private UpdateEndpoint endpoint;
 		// GET 
@@ -15,7 +20,7 @@ namespace Web.Tests.Books
 		[SetUp]
 		public void CanCreate()
 		{
-			endpoint = new UpdateEndpoint();
+			endpoint = new UpdateEndpoint(Session);
 		}
 
 		[Test]
@@ -23,6 +28,24 @@ namespace Web.Tests.Books
 		{
 			endpoint.Get(new UpdateBookLinkModel {Id = "123"});
 		}
+
+		[Test][Ignore] 
+		public void Get_GivenIdForBookThatLivesInSession_ShouldReturnViewModel_WithBooksDetails()
+		{
+			// get a book that is added to the session
+			var book = BookTestingHelper.GetBook();
+			book.Id = "69er";
+			Session.Store(book);
+			Session.SaveChanges();
+
+			// use book's id in the link model
+			var result = endpoint.Get(new UpdateBookLinkModel {Id = book.Id});
+
+			// verify the session was queried for the book's id
+			result.ShouldHaveDetailsFor(book);
+		}
+
+		// TODO - should return a book view model
 
 		// TODO - should blow up if ID Null
 
@@ -41,8 +64,63 @@ namespace Web.Tests.Books
 
 	public class UpdateEndpoint
 	{
-		public void Get(UpdateBookLinkModel updateBookLinkModel)
+		private readonly IDocumentSession session;
+
+		public UpdateEndpoint(IDocumentSession session)
 		{
+			this.session = session; 
+		}
+
+		public UpdateBookViewModel Get(UpdateBookLinkModel model)
+		{
+			// TODO - should controllers even see the domain entities?
+			var book = session.Load<Book>(model.Id);
+
+			return new UpdateBookViewModel(book);
+		}
+	}
+
+	public class UpdateBookViewModel
+	{
+		public UpdateBookViewModel(Book book)
+		{
+		}
+
+		public IList<String> Authors { get; set; }
+
+		public String Description { get; set; }
+
+		public String Id { get; set; }
+
+		public BookStatus Status { get; set; }
+
+		public String GenreName { get; set; }
+
+		public String GenreId { get; set; }
+
+		public String Title { get; set; }
+	}
+
+	public static class UpdateBookViewModelAssertions
+	{
+		public static void ShouldHaveDetailsFor (this UpdateBookViewModel model, Book book)
+		{
+			if (HasMatchingAuthors(model, book)
+				&& book.Description == model.Description
+				&& book.Genre.Name == model.GenreName
+				&& book.Genre.Id == model.GenreId
+				&& book.Id == model.Id
+				&& book.Status == model.Status
+				&& book.Title == model.Title)
+			{
+				return;
+			}
+		}
+
+		private static bool HasMatchingAuthors(UpdateBookViewModel model, Book book)
+		{
+			return model.Authors.Count() == book.Authors.Count()
+			       && book.Authors.All(model.Authors.Contains);
 		}
 	}
 }

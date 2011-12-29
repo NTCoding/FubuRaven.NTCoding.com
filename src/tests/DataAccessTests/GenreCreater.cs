feeb44ef4d8b120e13284537903825cd6bb0244e
NTCoding <Nick@NTCoding.co.uk>
2011-12-29 18:15:55 +0000

    Added CanCreate test for genre creater

--- src/tests/DataAccessTests/GenreCreater.cs
+++ src/tests/DataAccessTests/GenreCreater.cs
@@ -1,14 +1,35 @@
+using System.Linq;
+using DataAccessTests.Utilities;
+using Model;
+using Model.Services;
+using Model.Services.dtos;
 using NUnit.Framework;
 
 namespace DataAccessTests
 {
 	[TestFixture]
-	public class GenreCreater
+	public class GenreCreater : RavenTestsBase
 	{
+		private RavenDbGenreCreater creater;
+
+		[SetUp]
+		public void SetUp()
+		{
+			creater = new RavenDbGenreCreater(Session);
+		}
+
 		[Test]
 		public void CanCreate()
 		{
-			Assert.Fail();
+			var name = "blah";
+			creater.Create(new CreateGenreDto {Name = name});
+			Session.SaveChanges();
+
+			var fromSession = Session
+				.Query<Genre>()
+				.SingleOrDefault(g => g.Name == name);
+
+			Assert.IsNotNull(fromSession);
 		}
 
 		[Test]
